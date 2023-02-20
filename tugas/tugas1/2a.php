c1c9651eb4797f1fc5315f97c4de3dee63c409e8
ewokpenakluk <anggifirmansyah64@gmail.com>
2023-02-20 17:25:14 +0700

    mengumpulkan tugas 1

--- tugas/tugas1/2a.php
+++ tugas/tugas1/2a.php
@@ -1,24 +1,29 @@
 <?php
-$a = 52;
-$b = 5;
-$c = 2;
-$d = 75;
-$e = 20;
+$nrp = "52";
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tugas 2a</title>
+    <title>1b</title>
+    <style>
+
+        span {
+            font-weight: bold;
+        }
+    </style>
 </head>
+
 <body>
-    <p> aku adalah angka <b><?php echo "$a" ?></b></P>
-    <p> jika aku dikali <?php  print "$b"?>, maka aku sekarang jadi<b> <?php echo $a*$b ?></b> </p>
-    <p> jika aku dibagi <?php  print "$c"?>, maka aku sekarang jadi<b> <?php echo $a/$c ?></b> </p>
-    <p> jika aku ditambah <?php  print "$d"?>, maka aku sekarang jadi<b> <?php echo $a+$d ?></b> </p>
-    <p> jika aku dikurang <?php  print "$e"?>, maka aku sekarang jadi<b> <?php echo $a-$e ?></b> </p>
-    
+    <p>Aku adalah angka <span><?php echo "$nrp"; ?></span></p>
+    <p>Jika aku dikali 5, maka aku sekarang menjadi<span> <?php echo $nrp * 5 ?></span></p>
+    <p>Jika aku dibagi 2, maka aku sekarang menjadi<span> <?php echo $nrp / 2 ?></span></p>
+    <p>Jika aku ditambah 75, maka aku sekarang menjadi<span> <?php echo $nrp + 75 ?></span></p>
+    <p>Jika aku dikurang 20, maka aku sekarang menjadi<span> <?php echo $nrp - 20 ?></span></p>
 </body>
+
 </html>
