f9cca70e0fff1d0421a51b8ee566830409499b7b
ewokpenakluk <anggifirmansyah64@gmail.com>
2023-03-13 08:53:54 +0700

    mengumpulkan tugas 4

---
+++ tugas/tugas4/4a.php
@@ -0,0 +1,16 @@
+<?php
+$arrayEwok = ["asa", "epan", "cipta", "akan", "u", "esok"];
+
+echo "M" . $arrayEwok[0];
+echo " ";
+echo "d" . $arrayEwok[1] . "mu";
+echo " ";
+echo "di" . $arrayEwok[2] . "kan";
+echo " ";
+echo "oleh apa yang kau";
+echo " ";
+echo "kerj" . $arrayEwok[3];
+echo " ";
+echo "hari ini," . " " . "b" . $arrayEwok[4] . "kan";
+echo " ";
+echo  "b" . $arrayEwok[5];
---
+++ tugas/tugas4/4b.php
@@ -0,0 +1,35 @@
+<?php
+$PcHardware = ["Motherboard", "Processor", "Hard Disk", "Pc Cooler", "VGA Card", "SSD"];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h4>Macam-macam perangkat keras komputer</h4>
+    <ol>
+        <?php for ($i = 0; $i < count($PcHardware); $i++) { ?>
+            <li><?= $PcHardware[$i]; ?></li>
+        <?php } ?>
+    </ol>
+
+    <?php
+    array_unshift($PcHardware, "Card Reader", "Modem");
+    sort($PcHardware) ?>
+    <h4>Macam-macam perangkat keras komputer baru</h4>
+    <ol>
+        <?php for ($i = 0; $i < count($PcHardware); $i++) { ?>
+            <li><?= $PcHardware[$i]; ?></li>
+        <?php } ?>
+    </ol>
+
+</body>
+
+</html>
